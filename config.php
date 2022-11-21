f731d4d7403d533abcfe239b9f3e9e8914aec927
Ki55n <53857600+Ki55n@users.noreply.github.com>
2022-11-21 11:33:45 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME','Nithin');
-define('DB_PASSWORD','6364');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
 define('DB_NAME','bus-ticket-system');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
